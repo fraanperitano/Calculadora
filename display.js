5cd8be26b06a29e7bd4864220a007e85de7b8929
Francisco <franperitano1997@gmail.com>
2022-08-23 18:31:44 -0300

    archivos

---
+++ display.js
@@ -0,0 +1,17 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            dividir: '%',
+            multiplicar: 'x',
+            restar: '-', 
+        }
+    }
+
+}
